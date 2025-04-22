a6291ed45c997daca0a97d3b684059361e741530
rohan <rohan.hakke@nimapinfotech.com>
2025-04-22 11:55:34 +0530

    created sign up and jwt and also token creation

--- day2204/Auth/controllers/auth.js
+++ day2204/Auth/controllers/auth.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt")
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+const path = require("path")
+
 
 
 exports.signup =async (req,res)=>{
@@ -42,6 +46,61 @@ message:"user not register"
 }
 
 
-exports.login =async (req,res)=>{
+exports.login = async(req,res)=>{
+    try{
+        const {email,password} =req.body;
+        if(!email || !password){
+          return  res.status(400).json({
+                success:false,
+                message:"enter complete field"
+            })
+        }
+
+        const user = await User.findOne({email})
+        if(!user){
+          return  res.status(400).json({
+                success:false,
+                message:" user not register"
+            })
+        }
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role
+        }
+        if(await bcrypt.compare(password,user.password)){
+                    let token = jwt.sign(payload,process.env.JWT_SECRATE,{
+                        expiresIn:"1h"
+                    });
+                user.token = token;
+                user.password = undefined;
+                const options= {
+                            expiresIn:new Date(Date.now() +3*24*60*60*1000),
+                            httpOnly:true
+                }
+                res.cookie("token",token,options).status(200).json({
+                    success:true,
+                    token,
+                    user,
+                    message:"user login succesfuly!"
+                })
+
+
+
+        }else{
+            return res.status(500).json({
+                success:false,
+                message:"Password incurect"
+            })
+        }
+    }
+    catch(err){
+            console.log(err.message);
+            return res.status(400).json({
+                success:false,
+                message:"login faliled"
+            })
+            
+    }
         
 }
